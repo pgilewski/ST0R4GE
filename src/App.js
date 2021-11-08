936144d9c35bc7f0f16091f05354cf36d17c3c41
przemek <mrsmietnik@gmail.com>
2021-11-08 02:58:34 +0100

    dumb fix

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 /**
- * FIXME: This XML file does not appear to have any style information associated with it. The document tree is shown below.
+ * FIXME:
+ * - (on prod) This XML file does not appear to have any style information associated with it. The document tree is shown below.
+ *
+ * TODO:
+ * - no established internet connection handling
  */
 
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import React, { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 import Routes from './routes/Routes'
 
-import { AuthProvider } from './context/authContext';
+import { AuthProvider } from './context/authContext'
 
 function App() {
-  
-  const user = localStorage.getItem('user');
+  const [isNavbarDown, setIsNavbarDown] = useState(false)
+  const [isProfileDown, setIsProfileDown] = useState(false)
+
+  const bodyClick = () => {
+    setIsProfileDown(false)
+    setIsNavbarDown(false)
+  }
+
+  const user = localStorage.getItem('user')
   return (
     <AuthProvider user={user}>
       <Router>
-        <div className="App justify-items-center h-screen w-screen bg-gray-300">
-          <Navbar />
-          <div className="flex justify-center ">
-            <Routes/>
+        <div className="App justify-items-center h-screen w-screen ">
+          <Navbar
+            isNavbarDown={isNavbarDown}
+            setIsNavbarDown={setIsNavbarDown}
+            isProfileDown={isProfileDown}
+            setIsProfileDown={setIsProfileDown}
+          />
+          <div onClick={bodyClick} className="flex justify-center ">
+            <Routes />
           </div>
         </div>
       </Router>
     </AuthProvider>
-
-  );
+  )
 }
 
-export default App;
+export default App
