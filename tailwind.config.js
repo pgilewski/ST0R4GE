cb3c94623eb1601a8f81e069fddbc0eb324144ea
przemek <mrsmietnik@gmail.com>
2021-11-08 03:21:43 +0100

    tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,34 @@
 module.exports = {
+  important: true,
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
       '2xl': '1536px',
-    }
+    },
   },
+
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+      inset: ['checked'],
+      zIndex: ['hover', 'active'],
+    },
   },
   plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
 }
